568d83c0bdfc6f34e0a1f536a56424b86f48fb5c
Magesh <tkmagesh77@gmail.com>
2016-08-12 11:25:56 +0530

    Server setup

--- Day-03/async-samples.js
+++ Day-03/async-samples.js
@@ -11,22 +11,48 @@ var pgm = (function(){
 		console.log('[Service Consumer] result = ', result);
 	}
 
-	function addAsync(x,y){
+	function addAsync(x,y,onResult){
 		console.log('       [Service Provider] processing ', x, ' and ', y);
 		setTimeout(function(){
 			var result = x + y;
 			console.log('       [Service Provider] returning result');
-			return result;
+			if (typeof onResult === 'function')
+				onResult(result);
 		}, 3000);
 	}
 
 	function addAsyncClient(x,y){
 		console.log('[Service Consumer] triggering addAsync');
-		var result = addAsync(x,y);
-		console.log('[Service Consumer] result = ', result);
+		addAsync(x,y, function(result){
+			console.log('[Service Consumer] result = ', result);
+		});
+		
 	}
+
+	function addAsyncPromise(x,y){
+		console.log('       [Service Provider] processing ', x, ' and ', y);
+		var promise = new Promise(function(resolveFn, rejectFn){
+				setTimeout(function(){
+				var result = x + y;
+				console.log('       [Service Provider] returning result');
+				resolveFn(result);
+			}, 3000);
+		});
+		return promise;
+	}
+
+	function addAsyncPromiseClient(x,y){
+		console.log('[Service Consumer] triggering addAsyncPromise');
+		var promise = pgm.addAsyncPromise(x,y)
+		promise.then(function(result){
+		   console.log('[Service Consumer] result = ', result);
+		});
+	}
+
 	return {
 		addSyncClient : addSyncClient,
-		addAsyncClient : addAsyncClient
+		addAsyncClient : addAsyncClient,
+		addAsyncPromise : addAsyncPromise,
+		addAsyncPromiseClient : addAsyncPromiseClient
 	}
 })()
