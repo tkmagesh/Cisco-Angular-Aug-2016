c4b5a8b10ab677db5849dc658f4c8342ed7c18c8
Magesh <tkmagesh77@gmail.com>
2016-08-12 15:06:25 +0530

    Routing

---
+++ Day-03/10-BugTracker-Server/public/utils.js
@@ -0,0 +1,15 @@
+var utils = angular.module("utils", []);
+		utils.value("defaultTrimLength", 30);
+
+		utils.filter("trimText", function(defaultTrimLength){
+			return function(data, trimLength){
+				trimLength = trimLength || defaultTrimLength;
+				return data.length > trimLength ? data.substr(0,trimLength) + '...' : data;
+			}
+		});
+
+		utils.filter("elapsed", function(){
+			return function(data){
+				return moment(data).fromNow();
+			};
+		});
